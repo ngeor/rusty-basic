28ef4fb5f162651453d11288c2a03b99798cfdba
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-09-23 08:08:22 +0200

    Refactoring tests into separate files

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,18 +1,8 @@
 mod built_ins;
-mod constant;
 pub mod context;
-mod expression;
-mod for_loop;
-mod function_call;
-mod go_to;
-mod if_block;
 mod interpreter;
 mod io;
-mod on_error_goto;
-mod select_case;
 mod stdlib;
-mod sub_call;
-mod while_wend;
 
 #[cfg(test)]
 pub mod test_utils;
---
+++ src/interpreter/tests/on_error_goto.rs
@@ -0,0 +1,28 @@
+use crate::interpreter::test_utils::*;
+
+#[test]
+fn on_error_go_to_label() {
+    let input = r#"
+    ON ERROR GOTO ErrTrap
+    Environ "ShouldHaveAnEqualsSignInHereSomewhere"
+    PRINT "Will not print this"
+    SYSTEM
+    ErrTrap:
+        PRINT "Saved by the bell"
+    "#;
+    let interpreter = interpret(input);
+    assert_eq!(interpreter.stdlib.output, vec!["Saved by the bell"]);
+}
+
+#[test]
+fn on_error_go_to_label_with_dots_in_label_name() {
+    let input = r#"
+    ON ERROR GOTO Err.Trap
+    PRINT 1 / 0
+    SYSTEM
+    Err.Trap:
+        PRINT "Almost divided by zero"
+    "#;
+    let interpreter = interpret(input);
+    assert_eq!(interpreter.stdlib.output, vec!["Almost divided by zero"]);
+}

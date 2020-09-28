ff3d9f753572b4dc9b855d26e9a8a36fc2958d76
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-09-28 05:31:59 +0200

    removed unused code

--- src/interpreter/arguments.rs
+++ src/interpreter/arguments.rs
@@ -17,20 +17,6 @@ impl Arguments {
         Self::Unnamed(VecDeque::new())
     }
 
-    pub fn get(&self, param_name: &ParamName) -> Option<&Variant> {
-        match self {
-            Self::Named(map) => map.get(param_name),
-            Self::Unnamed(_) => None,
-        }
-    }
-
-    pub fn get_mut(&mut self, param_name: &ParamName) -> Option<&mut Variant> {
-        match self {
-            Self::Named(map) => map.get_mut(param_name),
-            Self::Unnamed(_) => None,
-        }
-    }
-
     pub fn push_unnamed(&mut self, arg: Variant) {
         match self {
             Self::Named(_) => panic!("Cannot push unnamed in Arguments::Named"),
@@ -45,20 +31,6 @@ impl Arguments {
                 None => {}
             },
             Self::Unnamed(_) => panic!("Cannot push named in Arguments::Unnamed"),
-        }
-    }
-
-    pub fn iter(&self) -> std::collections::vec_deque::Iter<Variant> {
-        match self {
-            Self::Named(_) => panic!("Not supported for Arguments::Named"),
-            Self::Unnamed(v) => v.iter(),
-        }
-    }
-
-    pub fn get_at(&self, index: usize) -> Option<&Variant> {
-        match self {
-            Self::Named(_) => panic!("Not supported for Arguments::Named"),
-            Self::Unnamed(v) => v.get(index),
         }
     }
 }

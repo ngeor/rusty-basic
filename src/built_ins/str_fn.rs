243638c5ac17f70275a9c248d957c746f778d45d
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-05-03 08:01:00 +0200

    STR$ function should print leading space for non-negative numbers (Fixes #86)

--- src/built_ins/str_fn.rs
+++ src/built_ins/str_fn.rs
@@ -14,13 +14,23 @@ pub mod interpreter {
     use crate::interpreter::interpreter_trait::InterpreterTrait;
     use crate::variant::Variant;
 
+    macro_rules! str_fmt {
+        ($e: expr, $zero: expr) => {
+            if $e >= $zero {
+                format!(" {}", $e)
+            } else {
+                format!("{}", $e)
+            }
+        };
+    }
+
     pub fn run<S: InterpreterTrait>(interpreter: &mut S) -> Result<(), QError> {
         let v: &Variant = &interpreter.context()[0];
         let result = match v {
-            Variant::VSingle(f) => format!("{}", f),
-            Variant::VDouble(f) => format!("{}", f),
-            Variant::VInteger(f) => format!("{}", f),
-            Variant::VLong(f) => format!("{}", f),
+            Variant::VSingle(f) => str_fmt!(*f, 0.0),
+            Variant::VDouble(f) => str_fmt!(*f, 0.0),
+            Variant::VInteger(f) => str_fmt!(*f, 0),
+            Variant::VLong(f) => str_fmt!(*f, 0),
             _ => panic!("unexpected arg to STR$"),
         };
         interpreter
@@ -32,12 +42,30 @@ pub mod interpreter {
 
 #[cfg(test)]
 mod tests {
-    use crate::assert_prints;
+    use crate::assert_prints_exact;
     use crate::interpreter::interpreter_trait::InterpreterTrait;
 
     #[test]
-    fn test_str_float() {
+    fn test_str_float_positive() {
         let program = r#"PRINT STR$(3.14)"#;
-        assert_prints!(program, "3.14");
+        assert_prints_exact!(program, " 3.14", "");
+    }
+
+    #[test]
+    fn test_str_float_negative() {
+        let program = r#"PRINT STR$(-3.14)"#;
+        assert_prints_exact!(program, "-3.14", "");
+    }
+
+    #[test]
+    fn test_str_integer_positive() {
+        let program = r#"PRINT STR$(42)"#;
+        assert_prints_exact!(program, " 42", "");
+    }
+
+    #[test]
+    fn test_str_integer_negative() {
+        let program = r#"PRINT STR$(-42)"#;
+        assert_prints_exact!(program, "-42", "");
     }
 }

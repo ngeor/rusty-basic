d377d0a0ec1f073c7d189bd2994f85bdbc8246cd
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-05-08 20:08:25 +0200

    Fixed bug in serialization of decimal numbers

--- src/variant/bits.rs
+++ src/variant/bits.rs
@@ -269,8 +269,35 @@ pub fn f64_to_bytes(f: f64) -> [u8; 8] {
 }
 
 fn f64_to_bits(value: f64) -> Vec<bool> {
+    match f64_abs_normalize_value(value) {
+        Some((absolute_value, initial_exponent)) => {
+            f64_to_bits_for_normalized_value(value < 0.0, absolute_value, initial_exponent)
+        }
+        None => {
+            // zero
+            [false; DOUBLE_BITS].to_vec()
+        }
+    }
+}
+
+macro_rules! int_to_bits_vec {
+    ($value: expr, $bits: expr, $bit_index: expr) => {{
+        let mut temp = $value;
+        while temp > 0 {
+            let remainder = temp % 2;
+            $bits.insert($bit_index, remainder == 1);
+            temp = temp / 2;
+        }
+    }};
+}
+
+fn f64_to_bits_for_normalized_value(
+    is_negative: bool,
+    absolute_value: f64,
+    initial_exponent: usize,
+) -> Vec<bool> {
     // msb -> lsb
-    let mut bits: Vec<bool> = vec![];
+    //
     //  1 bit for sign,
     //
     // 11 bit for exponent,
@@ -280,55 +307,92 @@ fn f64_to_bits(value: f64) -> Vec<bool> {
     // 1023 for bias
     //
     // 1.significant * 2 ^ exponent
-
-    let sign_bit: bool = value < 0.0_f64;
-    bits.push(sign_bit);
-
-    let absolute_value = value.abs();
+    let mut bits: Vec<bool> = vec![];
+    bits.push(is_negative);
 
     // create int_bits msb -> lsb
-    // e.g. int_bits for 4 will be [1, 0, 0]
-    let mut int_value: i64 = absolute_value.trunc() as i64;
-    let mut int_bits: Vec<bool> = vec![];
-    while int_value > 0 {
-        let remainder = int_value % 2;
-        int_bits.insert(0, remainder == 1);
-        int_value = int_value / 2;
-    }
+    // e.g. int_bits for 4 will be [1, 0, 0], but we remove the 1., so it will be [0, 0]
+    let int_bits = f64_int_bits(absolute_value);
+    let fraction_bits = f64_fractional_bits(absolute_value);
 
-    let mut fraction_value = absolute_value.fract();
-    let mut fraction_bits: Vec<bool> = vec![];
-    while fraction_bits.len() <= DOUBLE_BITS && (fraction_value - 1.0).abs() > 0.0 {
-        fraction_value = fraction_value * 2.0;
-        fraction_bits.push(fraction_value > 0.0);
+    let exponent_with_bias = (int_bits.len() as i32) + DOUBLE_BIAS - (initial_exponent as i32);
+    // insert the exponent bits
+    int_to_bits_vec!(exponent_with_bias, bits, 1);
+    // insert zeroes
+    while bits.len() < 1 + DOUBLE_EXPONENT_BITS {
+        bits.insert(1, false);
     }
-
-    let mut exponent = if int_bits.is_empty() {
-        0
-    } else {
-        (int_bits.len() as i32) - 1 + DOUBLE_BIAS
-    };
-    for _i in 0..DOUBLE_EXPONENT_BITS {
-        let remainder = exponent % 2;
-        exponent = exponent / 2;
-        bits.insert(1, remainder == 1);
-    }
-
-    if !int_bits.is_empty() {
-        int_bits.remove(0); // the 1. part is always implied
-    }
-    for _i in 0..DOUBLE_SIGNIFICANT_BITS {
-        let bit = if !int_bits.is_empty() {
-            int_bits.remove(0)
-        } else if !fraction_bits.is_empty() {
-            fraction_bits.pop().unwrap_or_default()
-        } else {
-            false
-        };
+    // make sure we didn't overflow the exponent bits
+    debug_assert_eq!(
+        1 + DOUBLE_EXPONENT_BITS,
+        bits.len(),
+        "Exponent bits overflow"
+    );
+    // insert the significant bits
+    for bit in int_bits
+        .into_iter()
+        .chain(fraction_bits.into_iter())
+        .take(DOUBLE_SIGNIFICANT_BITS)
+    {
         bits.push(bit);
     }
-
+    debug_assert_eq!(DOUBLE_BITS, bits.len());
     bits
+}
+
+fn f64_int_bits(absolute_value: f64) -> Vec<bool> {
+    let mut int_bits: Vec<bool> = vec![];
+    int_to_bits_vec!(absolute_value.trunc() as i64, int_bits, 0);
+    int_bits.remove(0); // it always starts with 1.
+    int_bits
+}
+
+fn f64_fractional_bits(absolute_value: f64) -> Vec<bool> {
+    let mut fraction_value = absolute_value.fract();
+    let mut fraction_bits: Vec<bool> = vec![];
+    while fraction_bits.len() <= DOUBLE_SIGNIFICANT_BITS {
+        if fraction_value >= 0.5 {
+            fraction_bits.push(true);
+            fraction_value = fraction_value * 2.0 - 1.0;
+        } else {
+            fraction_bits.push(false);
+            fraction_value = fraction_value * 2.0;
+        }
+    }
+    fraction_bits
+}
+
+fn f64_abs_normalize_value(value: f64) -> Option<(f64, usize)> {
+    let mut absolute_value = value.abs();
+    let mut exponent: usize = 0;
+    while absolute_value < 1.0 && exponent < (DOUBLE_BIAS as usize) {
+        absolute_value = absolute_value * 2.0;
+        exponent += 1;
+    }
+
+    if absolute_value < 1.0 {
+        None
+    } else {
+        Some((absolute_value, exponent))
+    }
+}
+
+#[cfg(test)]
+fn fmt_double_bits(bits: &[bool]) -> String {
+    let mut result = String::new();
+    fmt_bits_into_string(&mut result, &bits[0..1]);
+    result.push('|');
+    fmt_bits_into_string(&mut result, &bits[1..DOUBLE_EXPONENT_BITS + 1]);
+    result.push('|');
+    fmt_bits_into_string(&mut result, &bits[1 + DOUBLE_EXPONENT_BITS..]);
+    result
+}
+
+#[cfg(test)]
+fn fmt_bits_into_string(s: &mut String, bits: &[bool]) {
+    for bit in bits {
+        s.push(if *bit { '1' } else { '0' });
+    }
 }
 
 pub fn bytes_to_f64(bytes: &[u8]) -> f64 {
@@ -338,27 +402,38 @@ pub fn bytes_to_f64(bytes: &[u8]) -> f64 {
     let bits: Vec<bool> = lsb_bytes_to_msb_bits(bytes);
     debug_assert_eq!(bits.len(), DOUBLE_BITS);
     let sign = bits[0];
-    let exponent_bits = &bits[1..12];
-    let mut exponent: i32 = 0;
-    for i in 0..exponent_bits.len() {
-        if exponent_bits[i] {
-            exponent += 2_i32.pow((exponent_bits.len() - i) as u32 - 1)
+
+    let exponent_bits = &bits[1..DOUBLE_EXPONENT_BITS + 1];
+    debug_assert_eq!(DOUBLE_EXPONENT_BITS, exponent_bits.len());
+    let mut exponent_with_bias: i32 = 0;
+    for exponent_bit in exponent_bits.iter() {
+        exponent_with_bias *= 2;
+        if *exponent_bit {
+            exponent_with_bias += 1;
         }
     }
 
-    if exponent == 0 {
-        return 0.0;
-    }
+    // exponent_with_bias == 0 && F == 0 -> 0
+    // exponent_with_bias == 0 && F!= 0 -> subnormals
+    // exponent_with_bias == 0x7ff (all 1s) && F == 0 -> inf
+    // exponent_with_bias == 0x7ff (all 1s) && F != 0 -> NaN
 
-    let significant_bits = &bits[12..DOUBLE_BITS];
+    let significant_bits = &bits[1 + DOUBLE_EXPONENT_BITS..];
+    debug_assert_eq!(DOUBLE_SIGNIFICANT_BITS, significant_bits.len());
+
     // 1.significant * 2 ^ exponent - bias
     let mut result: f64 = 1.0;
     for i in 0..significant_bits.len() {
         if significant_bits[i] {
-            result += 1.0 / 2.0_f64.powi(i as i32 + 1);
+            result += 2.0_f64.powi(-(i as i32) - 1);
         }
     }
-    result *= 2.0_f64.powi(exponent - DOUBLE_BIAS);
+
+    if result == 1.0 && exponent_with_bias == 0 {
+        return 0.0;
+    }
+
+    result *= 2.0_f64.powi(exponent_with_bias - DOUBLE_BIAS);
     if sign {
         -result
     } else {
@@ -525,7 +600,7 @@ mod tests {
     }
 
     #[test]
-    fn test_f64_to_bytes() {
+    fn test_f64_to_bytes_ints() {
         assert_eq!(f64_to_bytes(-10.0), [0, 0, 0, 0, 0, 0, 0x24, 0xc0], "-10");
         assert_eq!(f64_to_bytes(-9.0), [0, 0, 0, 0, 0, 0, 0x22, 0xc0], "-9");
         assert_eq!(f64_to_bytes(-8.0), [0, 0, 0, 0, 0, 0, 0x20, 0xc0], "-8");
@@ -550,10 +625,103 @@ mod tests {
     }
 
     #[test]
-    fn test_bytes_to_f64() {
+    fn test_f64_to_bytes_ints_str() {
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(0.0)),
+            "0|00000000000|0000000000000000000000000000000000000000000000000000",
+            "0"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(1.0)),
+            "0|01111111111|0000000000000000000000000000000000000000000000000000",
+            "1"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(-1.0)),
+            "1|01111111111|0000000000000000000000000000000000000000000000000000",
+            "-1"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(2.0)),
+            "0|10000000000|0000000000000000000000000000000000000000000000000000",
+            "2"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(3.0)),
+            "0|10000000000|1000000000000000000000000000000000000000000000000000",
+            "3"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(4.0)),
+            "0|10000000001|0000000000000000000000000000000000000000000000000000",
+            "4"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(5.0)),
+            "0|10000000001|0100000000000000000000000000000000000000000000000000",
+            "5"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(6.0)),
+            "0|10000000001|1000000000000000000000000000000000000000000000000000",
+            "6"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(7.0)),
+            "0|10000000001|1100000000000000000000000000000000000000000000000000",
+            "7"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(10.0)),
+            "0|10000000010|0100000000000000000000000000000000000000000000000000",
+            "10"
+        );
+    }
+
+    #[test]
+    fn test_f64_to_bytes_doubles_str() {
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(0.5)),
+            "0|01111111110|0000000000000000000000000000000000000000000000000000",
+            "0.5"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(-0.5)),
+            "1|01111111110|0000000000000000000000000000000000000000000000000000",
+            "-0.5"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(0.375)),
+            "0|01111111101|1000000000000000000000000000000000000000000000000000",
+            "0.375"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(0.25)),
+            "0|01111111101|0000000000000000000000000000000000000000000000000000",
+            "0.25"
+        );
+        assert_eq!(
+            fmt_double_bits(&f64_to_bits(1.5)),
+            "0|01111111111|1000000000000000000000000000000000000000000000000000",
+            "1.5"
+        );
+    }
+
+    #[test]
+    fn test_bytes_to_f64_ints() {
         // let's try some integers
         for i in -MIN_INTEGER..MAX_INTEGER + 1 {
             let source: f64 = i as f64;
+            let bytes = f64_to_bytes(source);
+            let converted = bytes_to_f64(&bytes);
+            assert_eq!(source, converted, "{}", source);
+        }
+    }
+
+    #[test]
+    fn test_bytes_to_f64_double() {
+        for i in -100..100 {
+            let source: f64 = i as f64 * 0.1;
             let bytes = f64_to_bytes(source);
             let converted = bytes_to_f64(&bytes);
             assert_eq!(source, converted, "{}", source);

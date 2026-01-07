7d98df8f60ef2ff0dfa23e7964cb42922963838f
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2026-01-07 22:04:15 +0100

    refactor(parser): Implement the Parser trait directly on the char type

--- rusty_parser/src/tokens/char_parsers.rs
+++ rusty_parser/src/tokens/char_parsers.rs
@@ -10,8 +10,36 @@ where
     AnyChar.filter(predicate)
 }
 
+/// Parses the specific character.
+/// If it fails, it returns a non-fatal syntax error
+/// with a message like "Expected: z" (where 'z' is the character).
+///
+/// The [Parser] trait is implemented directly on the [char] type,
+/// so this function is just a helper to indicate we're returning it as a [Parser].
 pub fn specific(needle: char) -> impl Parser<RcStringView, Output = char, Error = ParseError> {
-    filter(move |ch| *ch == needle)
+    needle
+}
+
+impl Parser<RcStringView> for char {
+    type Output = char;
+    type Error = ParseError;
+
+    fn parse(&self, input: RcStringView) -> ParseResult<RcStringView, Self::Output, ParseError> {
+        if input.is_eof() {
+            default_parse_error(input)
+        } else {
+            let ch = input.char();
+            if ch == *self {
+                Ok((input.inc_position(), ch))
+            } else {
+                Err((
+                    false,
+                    input,
+                    ParseError::SyntaxError(format!("Expected: {}", self)),
+                ))
+            }
+        }
+    }
 }
 
 /// Parses any char.

34395293a69c91a58e510a16e219ab9eb445cfab
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2026-02-14 08:28:22 +0100

    Revert "Created dedicated parser IgnoringParser for skipping whitespace"
    This reverts commit ac14b620c2451807163d8b3bc34040eb780108a6.

--- rusty_pc/src/text/strings.rs
+++ rusty_pc/src/text/strings.rs
@@ -1,7 +1,5 @@
 use crate::many::{ManyCombiner, StringManyCombiner};
-use crate::{
-    IgnoringParser, IgnoringPredicate, InputTrait, Parser, ParserErrorTrait, one_p, read_p
-};
+use crate::{InputTrait, Parser, ParserErrorTrait, one_p, read_p};
 
 /// Parses one specific character and returns it as a string.
 pub fn one_char_to_str<I, E>(ch: char) -> impl Parser<I, Output = String, Error = E>
@@ -37,34 +35,4 @@ where
     C: ManyCombiner<char, O>,
 {
     read_p().filter(predicate).many(combiner)
-}
-
-/// A predicate that is used in order to skip whitespace.
-/// The whitespace is defined as spaces or tabs.
-pub struct WhitespaceIgnoringPredicate;
-
-impl IgnoringPredicate for WhitespaceIgnoringPredicate {
-    type Output = char;
-
-    fn test(&self, element: Self::Output) -> bool {
-        element == ' ' || element == '\t'
-    }
-}
-
-/// Creates a parser that skips while whitespace is encountered.
-/// At least one whitespace character must be found for the parser to succeed.
-pub fn whitespace_ignoring<E>() -> IgnoringParser<WhitespaceIgnoringPredicate, E>
-where
-    E: ParserErrorTrait,
-{
-    WhitespaceIgnoringPredicate.parser(false)
-}
-
-/// Creates a parser that skips while whitespace is encountered.
-/// The parser will only fail on EOF, as zero whitespace characters is also acceptable.
-pub fn opt_ws<E>() -> IgnoringParser<WhitespaceIgnoringPredicate, E>
-where
-    E: ParserErrorTrait,
-{
-    WhitespaceIgnoringPredicate.parser(true)
 }
